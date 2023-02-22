2148fa29d3bbe33e23beed9ae031555ee314f485
ardha78 <ardhakrs@gmail.com>
2023-02-22 17:03:27 +0800

    add dialog tambah barang

--- src/views/dashboard/tables/MasterTables.vue
+++ src/views/dashboard/tables/MasterTables.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/max-attributes-per-line -->
 <!-- eslint-disable no-undef -->
 <!-- eslint-disable vue/valid-v-model -->
 <template>
@@ -22,17 +23,115 @@
         <v-spacer />
         <v-btn
           color="primary"
-          dark
-          class="mb-2"
-          v-bind="props"
+          @click="dialog = true"
         >
           Tambah Barang
         </v-btn>
+
+        <v-dialog
+          v-model="dialog"
+          width="auto"
+        >
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Tambah Barang</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      label="Legal first name*"
+                      required
+                    />
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      label="Legal middle name"
+                      hint="example of helper text only on focus"
+                    />
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      label="Legal last name*"
+                      hint="example of persistent helper text"
+                      persistent-hint
+                      required
+                    />
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Email*"
+                      required
+                    />
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Password*"
+                      type="password"
+                      required
+                    />
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                  >
+                    <v-select
+                      :items="['0-17', '18-29', '30-54', '54+']"
+                      label="Age*"
+                      required
+                    />
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                  >
+                    <v-autocomplete
+                      :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                      label="Interests"
+                      multiple
+                    />
+                  </v-col>
+                </v-row>
+              </v-container>
+              <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="dialog = false"
+              >
+                Close
+              </v-btn>
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="dialog = false"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-card-title>
       <v-data-table
+        v-model="selected"
         :headers="headers"
         :items="barang"
-        v-model="selected"
         item-value="nama"
         show-select
       />
@@ -48,6 +147,7 @@
     data () {
       return {
         search: '',
+        dialog: false,
         headers: [
           { text: 'Id', value: 'id' },
           { text: 'Nama', value: 'nama' },
